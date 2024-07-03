854af6e499ea27e0c0ff2fe618e37e9b1e95dd18
Klein <kelan.jiang@seamoney.com>
2024-07-03 12:54:45 +0800

    Format Package.swift file.

--- Package.swift
+++ Package.swift
@@ -4,26 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftConcurrencySupport",
-    products: [
-      // Products define the executables and libraries a package produces, making them visible to other packages.
-      .library(
-        name: "SwiftConcurrencySupport",
-        targets: ["SwiftConcurrencySupport"]),
-    ], 
-    dependencies: [
-      .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0"))
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SwiftConcurrencySupport"),
-        .testTarget(
-            name: "SwiftConcurrencySupportTests",
-            dependencies: [
-              "SwiftConcurrencySupport",
-              .product(name: "RxCocoa", package: "RxSwift")
-            ]),
-    ]
+  name: "SwiftConcurrencySupport",
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "SwiftConcurrencySupport",
+      targets: ["SwiftConcurrencySupport"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0"))
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "SwiftConcurrencySupport"),
+    .testTarget(
+      name: "SwiftConcurrencySupportTests",
+      dependencies: [
+        "SwiftConcurrencySupport",
+        .product(name: "RxCocoa", package: "RxSwift"),
+      ]),
+  ]
 )
